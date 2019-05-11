c05babedd317b79909d7c4ba3999b9038f21a88c
Cabecinha84 <valter.cabecinha@gmail.com>
2019-05-11 20:19:31 +0100

    Added blocknumber info to Home dashboard

--- src/java/com/vaklinov/zcashui/DashboardPanel.java
+++ src/java/com/vaklinov/zcashui/DashboardPanel.java
@@ -567,7 +567,7 @@ public class DashboardPanel
 		}
 				
 		String text =langUtil.getString("panel.dashboard.network.blockchain.label",
-				percentage, info.lastBlockDate.toLocaleString(), info.numConnections );
+				percentage, info.lastBlockDate.toLocaleString(), info.numConnections, info.blockNumber);
 
 		this.networkAndBlockchainLabel.setText(text);
 		
